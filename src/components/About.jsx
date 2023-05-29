52b03d1961de4e59bd547c87e8e1b09bcb9f2551
ahmed <hetetlahma@gmail.com>
2023-05-29 22:32:47 +0300

    activate links

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,10 @@ import { about } from "../data";
 
 const About = () => {
   return (
-    <section className='section relative lg:before:content-pattern lg:before:absolute lg:before:top-[332px] lg:before:left-[180px] lg:after:content-rope lg:after:absolute lg:after:overflow-hidden lg:after:left-0 lg:after:right-0 lg:after:top-1/2 lg:after:flex lg:after:justify-center lg:after:items-center'>
+    <section
+      className='section relative lg:before:content-pattern lg:before:absolute lg:before:top-[332px] lg:before:left-[180px] lg:after:content-rope lg:after:absolute lg:after:overflow-hidden lg:after:left-0 lg:after:right-0 lg:after:top-1/2 lg:after:flex lg:after:justify-center lg:after:items-center'
+      id='about'
+    >
       <div className='container mx-auto'>
         <h2 className='section-title'>
           {about.title} <span className='dot'></span>
