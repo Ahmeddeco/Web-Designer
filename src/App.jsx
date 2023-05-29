18692d4dd8db70c13ba09aa9e06958737f124101
ahmed <hetetlahma@gmail.com>
2023-05-29 04:16:33 +0300

    first day

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header/>
+      <Hero />
+    </>
+  );
+};
+
+export default App;
